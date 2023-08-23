663adea33011701f5450fedea0756e1c7562a0cf
WGriffin23 <WGriffin23@uchicago.edu>
2023-08-23 16:51:21 -0500

    first stages of CNN construction

--- lightning_CNN.py
+++ lightning_CNN.py
@@ -7,3 +7,45 @@
 - 5k training images per class
 '''
 
+#%%
+# IMPORTS
+
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt 
+import torchvision 
+import torchvision.transforms as transforms
+import lightning.pytorch as pl
+import torch.nn.functional as F
+from lightning.pytorch import Trainer
+
+# %%
+# HYPERPARAMETERS
+
+num_epochs = 4
+batch_size = 96
+eta = .001 # learning rate
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse',
+'ship', 'truck')
+
+# %%
+# CLASS DESIGN
+'''
+Layer Guide:
+- Convolution/ReLU
+- Convolution/ReLU
+- Max Pool
+- Convolution/ReLU
+- Convolution/ReLU
+- Max Pool
+- Fully Connected
+- SoftMax **included in nn.CrossEntropyLoss()
+'''
+
+class CNN(pl.LightningModule): # inherit from LightningModule
+
+    # Constructor
+    def __init__(self):
+
+        super(CNN, self).__init__() # inherit constructor
+        
