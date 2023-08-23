2cc031366bf8e9390f2a6eee07745c87b0730291
WGriffin23 <WGriffin23@uchicago.edu>
2023-08-23 16:43:18 -0500

    added lightning CNN

---
+++ lightning_CNN.py
@@ -0,0 +1,9 @@
+# PyTorch Lightning CNN
+'''
+- Dataset is CIFAR-10
+- 60k 32x32 color images (3 channels)
+- 10 classes, 6k images per class
+- 1k testing images per class
+- 5k training images per class
+'''
+
